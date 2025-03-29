e382ec3d1ef3beddb64c34573498ec24164e9c1d
magicenvision <rjrose2003@gmail.com>
2025-03-28 20:30:04 -0400

    completed navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import NavBar from "./components/NavBar.jsx"
+import Hero from "./components/Hero.jsx"
+import Highlights from "./components/Highlights.jsx"
 import React from 'react'
 
 const App = () => {
   return (
     <main className='bg-black'>
-      
+      <NavBar />
+      <Hero />
+      <Highlights />
     </main>
   )
 }
