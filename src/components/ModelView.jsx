d39a92f1888097a6e4ee8192d15a9f24bf214082
magicenvision <rjrose2003@gmail.com>
2025-04-13 17:19:09 -0400

    final rendered model, too small toh

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import {PerspectiveCamera, View} from '@react-three/drei'
+import Lights from './Lights.jsx'
+import IPhone from './IPhone.jsx'
 
-const ModelView = () => {
+
+const ModelView = ({index, groupRef, gsapType, 
+  controlRef, setRotationState, item, size} ) => {
   return (
-    <div>ModelView</div>
+    <View 
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-500 h-full w-full
+    ${index === 2 ? 'right-100%' : ''}`}>
+      <ambientLight intensity={0.3}/>
+      <PerspectiveCamera makeDefault position={[0,0,4]}/>
+      <Lights />
+
+      <Suspense fallback={<div>Loading</div>}>
+      <IPhone/>
+      </Suspense>
+
+    </View>
   )
 }
 
